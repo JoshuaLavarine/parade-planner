b02c37a2e124efb232b508cb7fe750520b296492
Adam Niedzwiecki <adamn8142@gmail.com>
2019-02-13 14:51:29 -0700

    Set up classes
    Co-authored-by: Raechel Odom <odom.raechel@gmail.com>
    Co-authored-by: Joshua Lavarine <joshua@lavarine.com>
    Co-authored-by: Niraj Aryal <niroz.aryal11@gmail.com>

---
+++ src/CardContainer.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Card from './Card'
+
+
+
+class CardContainer extends Component {
+  constructor(){
+      super();
+  }
+
+    render(){
+      return(
+          <div>
+                WHAT ARE YOU DOING  
+          </div>
+      )
+  }     
+}
+
+
+
+export default CardContainer;
---
+++ src/FilterContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Filter from './Filter'
+
+class FilterContainer extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+      HEY MOTHA FACKA
+      </div>
+    )
+  }
+}
+
+export default FilterContainer;
