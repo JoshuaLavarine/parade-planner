8c71b059fe7e9d54aadf9660d230dbf56331dc6a
raechel <odom.raechel@gmail.com>
2019-02-14 19:10:08 -0700

    header component started and basic styling begun

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../Header.css';
+
+const Header = () => {
+  return (
+    <div className='Header'>
+      <h1>PARADE PLANNER</h1>
+      <h2>ULTIMATE MARDI GRAS EDITION</h2>
+    </div>
+  )
+}
+
+export default Header;
