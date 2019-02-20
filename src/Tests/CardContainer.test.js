074288cb51d27b41b310fa2fec6963679cb4600b
Joshua Lavarine <joshua@lavarine.com>
2019-02-20 13:02:33 -0700

    Test default state and snapshot in CardContainer.js
    Co-authored-by: Niraj Aryal <niroz.aryal11@gmail.com>

---
+++ src/Tests/CardContainer.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import CardContainer from '../Components/CardContainer';
+import { shallow } from 'enzyme';
+
+describe('App', () => {
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <CardContainer />
+    )
+  });
+	
+  it('should match the snapshot with all data passed in', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should have a proper default state', () => {
+    expect(wrapper.state()).toEqual({ id: -1, isExpanded: false });
+  });
+});
